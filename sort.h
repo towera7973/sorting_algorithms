6e926bbb2b8d2c60c1e98f7a907b5ae07f247ca2
towera7973 <towera7973@gmail.com>
2024-02-20 05:47:34 -0800

    the 2

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t my_partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -31,5 +33,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
+/* Prototypes for helper functions */
+void swap(int *a, int *b);
 #endif
